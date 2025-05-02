f4a6dfa984983796c9d0aa94776dd38ebd2e4621
Jailson Araujo <antonio.costa@squadra.com.br>
2025-05-02 14:34:26 -0300

    novas atuali...

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,11 +1,13 @@
 describe('TESTANDO', () => {
     it('Testes 1', () => {
       cy.visit('https://www.google.com/');
+      cy.wait(3000)
       cy.contains('Estou com sorte')
     })
 
     it('Testes 2', () => {
       cy.visit('https://www.google.com/');
+      cy.wait(3000)
       cy.contains('Pesquisa Google')
     })
 });
